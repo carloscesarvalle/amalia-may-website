16b871a4d893b92f608c6b4dbb1bec1c694d7c49
carloscesarvalle <carloscesarvalle@gmail.com>
2020-11-06 12:20:03 -0500

    some body tect from client addded

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+gsap.from('.text-for-bg-image', {y: '-15vh', delay: 1});
 gsap.from('.reel', {y: '15vh', delay: 1});
 gsap.from('.news', {y: '15vh', delay: 2});
 gsap.from('.update', {y: '15vh', delay: 3});
